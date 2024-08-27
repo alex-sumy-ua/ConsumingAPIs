895a3e2419ecd332938390f2c7b8c9b1495383fa
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-08-27 16:21:51 +0100

    Create ApiResponse.java

---
+++ src/main/java/org/northcoders/ApiResponse.java
@@ -0,0 +1,45 @@
+package org.northcoders;
+
+import org.northcoders.model.Book;
+
+import java.util.List;
+
+public class ApiResponse {
+    private String status;
+    private int code;
+    private int total;
+    private List<Book> data;
+
+    // Getters and setters
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public List<Book> getData() {
+        return data;
+    }
+
+    public void setData(List<Book> data) {
+        this.data = data;
+    }
+}
