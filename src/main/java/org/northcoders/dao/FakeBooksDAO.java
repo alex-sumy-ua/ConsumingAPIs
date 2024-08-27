cc84c343293cba25358c5e5591352584ba1cb02c
Emily Rhodes <erhodes.345@gmail.com>
2024-08-27 12:32:37 +0100

    add Task 16 comment out

--- src/main/java/org/northcoders/dao/FakeBooksDAO.java
+++ src/main/java/org/northcoders/dao/FakeBooksDAO.java
@@ -4,5 +4,21 @@ public class FakeBooksDAO {
 
     // url = https://fakerapi.it/api/v1/books
 
+//    public Book getResponseFromAPI (String url) {
+//
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//               // .uri(URI.create("https://fakerapi.it/api/v1/books"))
+//                .build();
+//
+//        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        // do we need a status code exception???? if we
+//        if (response.statusCode = 200) {
+//            return objectMapper.readValue(response)
+//        }
+    }
     
 }
