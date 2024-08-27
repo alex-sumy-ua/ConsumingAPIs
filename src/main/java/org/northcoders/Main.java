5d2b0255cea2f45eea9186a93fa7141a29989b8d
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-08-27 16:18:43 +0100

    Update Main.java

--- src/main/java/org/northcoders/Main.java
+++ src/main/java/org/northcoders/Main.java
@@ -2,14 +2,16 @@ package org.northcoders;
 
 import org.northcoders.dao.FakeBooksDAO;
 import org.northcoders.model.Book;
-
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
 
-    //    FakeBooksDAO dao = new FakeBooksDAO();
-      //  List<Book> books = dao.getBooks();
-        //books.forEach(System.out::println);
+            FakeBooksDAO dao = new FakeBooksDAO();
+            List<Book> books = dao.getBooks();
+
+            books.forEach(System.out::println);
     }
+
 }
