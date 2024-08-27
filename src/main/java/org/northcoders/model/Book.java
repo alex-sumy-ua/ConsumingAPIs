d8c53a0a394c7194254f470c60282980a573fe27
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-08-27 16:19:29 +0100

    Update Book.java

--- src/main/java/org/northcoders/model/Book.java
+++ src/main/java/org/northcoders/model/Book.java
@@ -7,20 +7,9 @@ public record Book(int id,
                    String author,
                    String genre,
                    String description,
-                   Long isbn,
+                   String isbn,
                    String image,
                    Date published,
                    String publisher) {
 
-    public Book(int id, String title, String author, String genre, String description, Long isbn, String image, Date published, String publisher) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.description = description;
-        this.isbn = isbn;
-        this.image = image;
-        this.published = published;
-        this.publisher = publisher;
-    }
 }
